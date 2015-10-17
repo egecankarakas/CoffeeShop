aee0c816ad25933e78fe9366b2fbb91032523a1e
egecankarakas <egecan.karakas@ozu.edu.tr>
2015-10-17 17:59:57 +0300

    asdf

--- src/Server/Engine.java
+++ src/Server/Engine.java
@@ -1,14 +1,25 @@
 package Server;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.*;
 public class Engine {
-	public void main(String [] args) {
-		
-		
-		
-		
-		
+	public void main(String [] args) throws IOException {
+		ServerSocket listener = new ServerSocket(9090);
+        try {
+            while (true) {
+                Socket socket = listener.accept();
+                try {
+                    PrintWriter out =
+                        new PrintWriter(socket.getOutputStream(), true);
+                    
+                } finally {
+                    socket.close();
+                }
+            }
+        }
+        finally {
+            listener.close();
+        }
 	}
-	
-	
-
 }
