333739366e2ba472135762513447f42a14393615
Erdi Gültekin <erdi.gultekin@ozu.edu.tr>
2015-10-17 17:59:58 +0300

    teacher gui

--- src/Server/Teacher.java
+++ src/Server/Teacher.java
@@ -1,21 +1,35 @@
 package Server;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class Teacher {
-	
-	public void readFromClient(String str){
-		System.out.println("The price entered by the Client: " +str);
-		//Firstly we need to control the price entered as a readable Integer or
-		//some other String like "asdf". I will evalute unreadable strings as 0
-		
-		Double price = 0.0;
-		try {
-			price = Double.parseDouble(str);
-		}catch(java.lang.NumberFormatException ne){
-			System.out.println("Illegat format exception Caught. Price Set to 0.0");
-		}
-		System.out.println("Price readed is :" +price);
-		
-		//For a starting point I will...
-	}
-	
+   public static void main(String[] args) {
+      
+      JButton okButton = new JButton("Run");
+      ButtonHandler listener = new ButtonHandler();
+      okButton.addActionListener(listener);
+      JPanel content = new JPanel();
+      JLabel title = new JLabel();
+      title.setText("Coffee Shop Simulation Management Consolex");
+      content.setLayout(new BorderLayout());
+      content.add(okButton, BorderLayout.SOUTH);
+      content.add(title, BorderLayout.NORTH);
+
+      JFrame window = new JFrame("Coffee Shop Simulation Management Console");
+      window.setContentPane(content);
+      window.setSize(500,500);
+      window.setLocation(100,100);
+      window.setVisible(true);
+
+   }
+   
+
+   
+   private static class ButtonHandler implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+//do-nothing
+      }
+   }
+   
 }
