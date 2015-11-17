96325f5e6472cd26026bcc381651ca5d4b74d67c
Erdi Gültekin <erdi.gultekin@ozu.edu.tr>
2015-11-17 22:51:45 +0200

    Teacher interface is completed.

--- src/Server/Engine.java
+++ src/Server/Engine.java
@@ -4,7 +4,17 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.*;
 public class Engine {
-	public void main(String [] args) throws IOException {
+	
+	
+	public Engine() {
+	
+	}
+	
+	public void updatePlayerList () {
+		
+	}
+	
+	public void updateDataList() {
 		
 	}
 }
--- src/Server/Teacher.java
+++ src/Server/Teacher.java
@@ -6,26 +6,40 @@ import javax.swing.*;
 public class Teacher {
    public static void main(String[] args) {
       
-      JButton okButton = new JButton("Run");
+      JButton okButton = new JButton("Run Simulation for the Next Day");
       ButtonHandler listener = new ButtonHandler();
       okButton.addActionListener(listener);
       
       JLabel title = new JLabel();
-      title.setText("Coffee Shop Simulation Management Console");
+      title.setText("Here is a list of connected users and their data status:");
+     
+      JLabel day = new JLabel();
+      day.setText("This is day 1.");
       
-      JList userList = new JList();
+      String playerList[] ={"User A", "User B", "User C", "User D", "User E"};
+      
+      JList userList = new JList(playerList);
+      
+      String dataStatus[] ={"OK", "-", "OK", "OK", "-"};
+      
+      JList data = new JList(dataStatus);
+      
       
       JPanel content = new JPanel();
       content.setLayout(new BorderLayout());
       content.add(okButton, BorderLayout.SOUTH);
       content.add(title, BorderLayout.NORTH);
-      content.add(userList, BorderLayout.CENTER);
+      content.add(day, BorderLayout.EAST);
+      content.add(data, BorderLayout.CENTER);
+      content.add(userList, BorderLayout.WEST);
 
       JFrame window = new JFrame("Coffee Shop Simulation Management Console");
       window.setContentPane(content);
       window.setSize(500,500);
       window.setLocation(100,100);
       window.setVisible(true);
+      
+      window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
    }
    
